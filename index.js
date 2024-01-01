da7b1e83aa4c585741c9de6678581d395a256667
Tarik <tarikmerrylees@gmail.com>
2024-01-01 21:59:53 +0000

    add a number of moves option

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const attack = document.getElementById("attack");
 const number = document.getElementById("number");
 const startStop = document.getElementById("startStop");
+const countViewer = document.getElementById("countViewer");
+const infinity = document.getElementById("infinity");
+const zeroCount = document.getElementById("zeroCount");
+const plusFive = document.getElementById("plusFive");
+const minusFive = document.getElementById("minusFive");
 
 let lowestTime = 5;
 let highestTime = 8; 
 let moveCount = 0;
+let countLimit = Infinity;
 let lastNum;
 let intervalId;
+
+infinity.addEventListener('click', () => {
+  countLimit = Infinity
+  countViewer.innerText = "∞";
+});
+
+zeroCount.addEventListener('click', () => {
+  countLimit = 0
+  countViewer.innerText = "0";
+});
+
+plusFive.addEventListener('click', () => {
+  if (countLimit === Infinity) {
+    countLimit = 0
+  }
+  countLimit += 5
+  countViewer.innerText = countLimit;
+});
+
+minusFive.addEventListener('click', () => {
+  if (countLimit === Infinity) {
+    countLimit = 0
+  }
+
+  if (countLimit > 0) {
+    countLimit -= 5
+    countViewer.innerText = countLimit;
+  }
+});
 
 const randomiseStandardorReverse = () => Math.random() < 0.5 ? "standard" : "reverse";
 const randomiseAttackOrDefend = () => Math.random() < 0.5 ? "Attack" : "Block";
@@ -26,6 +61,13 @@ const speak = (text) => {
 };
 
 const updateUI = () => {
+  if (moveCount !== Infinity) {
+    if (moveCount > countLimit) {
+      stop();
+      speak(`Set Complete! Well done apprentice.`);
+      return;
+    }
+  }
   const num = randomOneToNine();
   while (num === lastNum) num = randomOneToNine(); // Prevent the same number twice in a row
   number.innerText = num;
@@ -46,18 +88,22 @@ const updateUI = () => {
 };
 
 const start = () => {
-  startStop.innerText = "STOP";
-  startStop.removeEventListener('click', start);
-  startStop.addEventListener('click', stop);
+  if (countLimit === 0) alert("Please select a number of moves");
 
-  updateUI(); // Initial update
-  intervalId = setInterval(() => {
-    updateUI(); // Repeated updates
-
-    // Adjust the interval time
-    clearInterval(intervalId);
-    intervalId = setInterval(updateUI, generateRandomTimeInterval());
-  }, generateRandomTimeInterval());
+  if (countLimit !== 0) {
+    startStop.innerText = "STOP";
+    startStop.removeEventListener('click', start);
+    startStop.addEventListener('click', stop);
+    
+    updateUI(); // Initial update
+    intervalId = setInterval(() => {
+      updateUI(); // Repeated updates
+      
+      // Adjust the interval time
+      clearInterval(intervalId);
+      intervalId = setInterval(updateUI, generateRandomTimeInterval());
+    }, generateRandomTimeInterval());
+  }
 };
 
 
@@ -66,6 +112,7 @@ const stop = () => {
   startStop.innerText = "START";
   startStop.removeEventListener('click', stop);
   startStop.addEventListener('click', start);
+  moveCount = 0;
 };
 
 startStop.addEventListener('click', start);
